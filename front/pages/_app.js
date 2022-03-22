57052d2cd7465fed3ce8d0dcba49f8dc3b921fe2
hyeongyu shin <rkrkdldkd@gmail.com>
2022-03-23 01:38:44 +0900

    🎉  Begin a project

---
+++ front/pages/_app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/style.scss";
+
+const NodeBird = ({ Component }) => {
+  return (
+    <div>
+      <Component></Component>
+    </div>
+  );
+};
+
+export default NodeBird;
---
+++ front/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../layouts/layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="hi">hello world</div>
+    </Layout>
+  );
+};
+
+export default Home;
